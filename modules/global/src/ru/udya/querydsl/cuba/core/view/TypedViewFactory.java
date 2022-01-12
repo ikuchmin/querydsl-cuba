423395fa4b705e8d8045e655fe255bb9e1f9e9f9
Ilya Kuchmin <kuchmin@haulmont.com>
2022-01-12 14:04:43 +0400

    Add TypedViewFactory

--- modules/global/src/ru/udya/querydsl/cuba/core/view/TypedViewFactory.java
+++ modules/global/src/ru/udya/querydsl/cuba/core/view/TypedViewFactory.java
@@ -1,4 +1,34 @@
 package ru.udya.querydsl.cuba.core.view;
 
+import com.haulmont.cuba.core.entity.Entity;
+import com.querydsl.core.types.Path;
+
+import java.util.List;
+import java.util.function.Function;
+
 public class TypedViewFactory {
+
+    public <Q extends Path<? extends Entity<?>>>
+    TypedViewBuilder<Q> view(Q viewType) {
+        return this.<Q>builder().view(viewType);
+    }
+
+    public <Q extends Path<? extends Entity<?>>>
+    TypedViewBuilder<Q> view(Q viewType, Path<?>... properties) {
+        return this.<Q>builder().view(viewType, properties);
+    }
+
+    public <Q extends Path<? extends Entity<?>>>
+    TypedViewBuilder<Q> view(Q viewType, List<Path<?>> properties) {
+        return this.<Q>builder().view(viewType, properties);
+    }
+
+    public <Q extends Path<? extends Entity<?>>>
+    TypedViewBuilder<Q> view(Q viewType, Function<Q, List<Path<?>>> properties) {
+        return this.<Q>builder().view(viewType, properties);
+    }
+
+    protected  <Q extends Path<? extends Entity<?>>> TypedViewBuilder<Q> builder() {
+        return new TypedViewBuilder<>();
+    }
 }
